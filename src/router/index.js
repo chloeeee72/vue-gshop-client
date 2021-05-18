5fc2463715072c7086aa571c9c38433bb97e7191
chloe <1935513424a@gmaiil.com>
2021-05-18 21:21:59 +0800

    add LoginPage

--- src/router/index.js
+++ src/router/index.js
@@ -4,33 +4,50 @@ import MSite from "../pages/MSite/MSite.vue";
 import Order from "../pages/Order/Order.vue";
 import Profile from "../pages/Profile/Profile.vue";
 import Search from "../pages/Search/Search.vue";
+import Login from "../pages/Login/Login.vue"
 Vue.use(Router);
 
 export default new Router({
-  routes: [
-    {
+  routes: [{
       path: "/",
       redirect: "/msite"
     },
     {
       path: "/msite",
       name: "MSite",
-      component: MSite
+      component: MSite,
+      meta: {
+        showFooter: true
+      }
     },
     {
       path: "/order",
       name: "Order",
-      component: Order
+      component: Order,
+      meta: {
+        showFooter: true
+      }
     },
     {
       path: "/profile",
       name: "Profile",
-      component: Profile
+      component: Profile,
+      meta: {
+        showFooter: true
+      }
     },
     {
       path: "/search",
       name: "Search",
-      component: Search
+      component: Search,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: "/login",
+      name: "Login",
+      component: Login
     }
   ]
 });
